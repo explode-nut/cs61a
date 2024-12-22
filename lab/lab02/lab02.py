953e5c7ee06060729aed812dbc0a5f77ea43eaf8
shimmer <1072215361@qq.com>
2024-12-22 22:39:15 +0800

    lab02完成

--- lab/lab02/lab02.py
+++ lab/lab02/lab02.py
@@ -13,8 +13,7 @@ def lambda_curry2(func):
     >>> lambda_curry2(mod)(123)(10)
     3
     """
-    "*** YOUR CODE HERE ***"
-    return ______
+    return lambda x: lambda y: func(x, y)  
 
 
 def lambda_curry2_syntax_check():
@@ -55,7 +54,15 @@ def count_cond(condition):
     >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
     8
     """
-    "*** YOUR CODE HERE ***"
+    def cond(n):
+        i = 1
+        count = 0
+        while i <= n:
+            if condition(n, i):
+                count += 1
+            i += 1
+        return count
+    return cond
 
 
 def composer(f, g):
