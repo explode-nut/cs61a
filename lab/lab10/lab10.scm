5ff9519932317961d7b8f531c84acda5be3b855b
shimmer <1072215361@qq.com>
2024-12-31 17:52:55 +0800

    lab10完成

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,9 +1,24 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+	(cond
+		((< num1 num2) -1)
+		((= num1 num2) 0)
+		(else 1)
+	)
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+	(lambda (x) (+ num x))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+	(lambda (x) (f (g x)))
+)
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+	(if (= (modulo exp 2) 0)
+		(if (= exp 2) (square base) (square (pow base (/ exp 2))))
+		(* base (pow base (- exp 1)))
+	)
+)
