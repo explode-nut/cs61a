4450fed3ac70e934d8ffa859dee63a49d68650e1
shimmer <1072215361@qq.com>
2025-01-18 18:46:50 +0800

    scheme problem11完成

--- proj/scheme/scheme_eval_apply.py
+++ proj/scheme/scheme_eval_apply.py
@@ -35,18 +35,13 @@ def scheme_eval(expr, env, _=None):  # Optional third argument is ignored
     else:
         # BEGIN PROBLEM 3
         if isinstance(first, Pair):
-            print('DEBUG:', first)
             p = scheme_eval(first, env)
-            print('DEBUG:', 1111)
         else:
             # print('DEBUG:', first)
             p = env.lookup(first)
         validate_procedure(p)
         args = rest.map(lambda exp, en=env: scheme_eval(exp, en))
-        print('DEBUG:', p)
-        print('DEBUG:', args)
-        print('DEBUG:', env)
-        return scheme_apply(p, args, env if isinstance(p, BuiltinProcedure) else p.env)
+        return scheme_apply(p, args, env if isinstance(p, BuiltinProcedure) or isinstance(p, MuProcedure) else p.env)
         # END PROBLEM 3
 
 
@@ -76,7 +71,8 @@ def scheme_apply(procedure, args, env):
         # END PROBLEM 9
     elif isinstance(procedure, MuProcedure):
         # BEGIN PROBLEM 11
-        "*** YOUR CODE HERE ***"
+        child = env.make_child_frame(procedure.formals, args)
+        return eval_all(procedure.body, child)
         # END PROBLEM 11
     else:
         assert False, "Unexpected procedure: {}".format(procedure)
