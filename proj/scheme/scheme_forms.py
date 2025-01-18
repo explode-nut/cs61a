2757640d48524be502b10b7ff580bd3585be7315
shimmer <1072215361@qq.com>
2025-01-18 17:40:28 +0800

    scheme problem10完成

--- proj/scheme/scheme_forms.py
+++ proj/scheme/scheme_forms.py
@@ -43,7 +43,8 @@ def do_define_form(expressions, env):
     elif isinstance(signature, Pair) and scheme_symbolp(signature.first):
         # defining a named procedure e.g. (define (f x y) (+ x y))
         # BEGIN PROBLEM 10
-        "*** YOUR CODE HERE ***"
+        env.define(signature.first, do_lambda_form(Pair(signature.rest, expressions.rest), env))
+        return signature.first
         # END PROBLEM 10
     else:
         bad_signature = signature.first if isinstance(signature, Pair) else signature
